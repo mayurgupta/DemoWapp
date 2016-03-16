ffe143bc3398b97f1dcca82a88d4643b2d08f656
laxmideshmukh <laxmi.deshmukh1010@gmail.com>
2016-03-16 23:41:38 +0530

    vcvccvbbbb

---
+++ src/main/webapp/app/components/register/registerController.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Laxmi on 3/14/2016.
+ */
+truckTransApp.controller('RegisterController', ['$scope', 'RegisterService', function ($scope, RegisterService) {
+    $scope.init = function () {
+
+    };
+
+
+    $scope.addUser = function (userDetails) {
+    	console.log(userDetails);
+        var itemToSave = {};
+        itemToSave.name = userDetails.name;
+        itemToSave.password = userDetails.password;
+        itemToSave.confirmPassword = userDetails.confirmPassword;
+        itemToSave.role = userDetails.role;
+        itemToSave.email = userDetails.email;
+        itemToSave.address=userDetails.address;
+
+
+
+        RegisterService.addUser(itemToSave).then(function (response) {
+            console.log(response);
+        })
+    };
+
+
+    $scope.init();
+
+}]);
---
+++ src/main/webapp/app/components/register/registerService.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Laxmi on 3/14/2016.
+ */
+truckTransApp.service('RegisterService', function ($http) {
+    var registerService = {
+
+        addUser: function (userData) {
+            // showProcessDialog();
+            console.log(userData);
+            var promise = $.ajax({
+                url: Server + "/truckTrans/register",
+                data: JSON.stringify(userData),
+                type: "POST",
+                contentType: "application/json",
+                dataType: "json"
+            }).then(function (response) {
+                // The then function here is an opportunity to modify the response
+
+                return response;
+            });
+            // Return the promise to the controller
+            return promise;
+        }
+
+    };
+
+
+    return registerService;
+
+});
