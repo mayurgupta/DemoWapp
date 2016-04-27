83dd62380319fd4106c9d1968bd9d0bdb1084de4
Mayur Gupta <mayurguptacs59@gmail.com>
2016-04-28 02:18:56 +0530

    history download invoice

--- src/main/java/com/trucktrans/web/controllers/HistoryController.java
+++ src/main/java/com/trucktrans/web/controllers/HistoryController.java
@@ -5,12 +5,17 @@ package com.trucktrans.web.controllers;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import com.trucktrans.entity.dto.TransComQuotesDTO;
+import com.trucktrans.services.IHistoryService;
 
 /**
  * @author mgupta
@@ -19,32 +24,35 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("request")
 @Path("/history")
-public class HistoryController extends AbstractRestController<Object>{
+public class HistoryController extends AbstractRestController<TransComQuotesDTO>{
 
-	public HistoryController(Class<Object> t) {
-		super(t);
+	public IHistoryService getHistoryService() {
+		return historyService;
 	}
+
+	@Autowired
+	IHistoryService historyService;
 	
 	public HistoryController() {
-		super(Object.class);
+		super(TransComQuotesDTO.class);
 	}
 	
 	@GET
-    @Path("/acceptedquotes")
-    @Produces({ MediaType.APPLICATION_JSON })
-	public Response myAccptQuotes(){
-		return Response.ok().build();
-	}
-	
-	
-	@GET
-    @Path("/declinedquotes")
-    @Produces({ MediaType.APPLICATION_JSON })
-	public Response myDeclndQuotes(){
-		return Response.ok().build();
-	}
-	
-	@GET
+    @Path("/acceptedquotes")					   //
+    @Produces({ MediaType.APPLICATION_JSON })	   //
+	public Response myAccptQuotes(){			   //	
+		return Response.ok().build();			   //
+	}											   //
+	                                               //
+	                                               //we can combine these two methods into one method
+	@GET                                           //
+    @Path("/declinedquotes")                       //
+    @Produces({ MediaType.APPLICATION_JSON })      //
+	public Response myDeclndQuotes(){              //Load these methods when history page opens
+		return Response.ok().build();              //
+	}                                              //
+	                                                            
+	@GET                                                        
     @Path("/orderfrmquote/{quoteid}")
     @Produces({ MediaType.APPLICATION_JSON })
 	//this is for transporter
@@ -56,9 +64,8 @@ public class HistoryController extends AbstractRestController<Object>{
 	@GET
     @Path("/invoice/{quoteid}")
     @Produces({ MediaType.APPLICATION_JSON })
-	public Response downldInvoice(){
-		//TODO write code to generate PDF
-		return Response.ok().build();
+	public Response downloadInvoice(@PathParam("quoteid") Long quoteId){
+		return Response.ok(getHistoryService().generateInvoice(quoteId)).build();
 	}
 	
 }
