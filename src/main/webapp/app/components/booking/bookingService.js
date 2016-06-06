e1a1707afdec27d1c137da331320ab583c826179
laxmideshmukh <laxmi.deshmukh1010@gmail.com>
2016-06-07 00:20:03 +0530

    Update bookingService.js

--- src/main/webapp/app/components/booking/bookingService.js
+++ src/main/webapp/app/components/booking/bookingService.js
@@ -1,7 +1,25 @@
 truckTransApp.service('BookingService', function ($http) {
         var bookingService = {
 
+            confirmBookingBeforeLogin: function (bookingData) {
+               // showProcessDialog();
+                console.log(bookingData);
+                var promise = $.ajax({
+                    url: Server + "ign/init/planguesttransportation",
+                    data: JSON.stringify(bookingData),
+                    type: "POST",
+                    contentType: "application/json",
+                    dataType: "json"
+                }).then(function (response) {
+                    // The then function here is an opportunity to modify the response
+
+                    return response;
+                });
+                // Return the promise to the controller
+                return promise;
+            },
             confirmBooking: function (bookingData) {
+                    
                // showProcessDialog();
                 console.log(bookingData);
                 var promise = $.ajax({
